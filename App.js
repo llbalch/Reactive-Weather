0d6589e5d80e07546047ce01393e04bf8cfd3572
Lauren Balch <llbalch@gmail.com>
2025-05-27 12:17:43 -0500

    Developed WeatherCard componenent to render city name, temp and weather conditions

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
-
+import WeatherCard from './components/WeatherCard.js'
+import {cities} from './data.js'
 
 function App() {
     return (
@@ -8,10 +9,10 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
+                {cities.map((city)=> 
+                < WeatherCard key={city.id} city ={city} />
+                )}
                 
-
-
             </div>
             
         </>
