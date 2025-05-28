6b05067f6617f8797d0675f17b93ecd7b1bee677
Lauren Balch <llbalch@gmail.com>
2025-05-28 10:17:57 -0500

    added other weather icons to weathercard

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,13 +1,27 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
+import cloudy from "../assets/Cloudy.svg"
+import partlyCloudy from "../assets/PartlyCloudy.svg"
+import rainy from "../assets/Rainy.svg"
 import { preinitModule } from "react-dom";
 
 
 function WeatherCard(props) {
+let icon;
+if (props.data.forecast === "Sunny"){
+  icon = sunny;
+} else if (props.data.forecast === "Cloudy"){
+  icon = cloudy;
+} else if (props.data.forecast === "Partly Cloudy"){
+  icon = partlyCloudy;
+} else if (props.data.forecast === "Rainy"){
+  icon = rainy;
+}
+
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {icon} alt={props.data.forecast} id = "icon"/>
         </div>
         <div className="card-body">
             <h3 className="card-title">{props.city.city}</h3>
