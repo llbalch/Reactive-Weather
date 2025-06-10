df13dd923d0b5d012ad4294eedc0ddfb0f4ceb5f
Lauren Balch <llbalch@gmail.com>
2025-06-10 14:00:59 -0500

    added location functionality

--- components/Location.js
+++ components/Location.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { getWeatherIcon } from "./helpers/WeatherIcon";
+
+function Location() {
+    const [location, setLocation] = useState("Dallas")
+    const [weather, setWeather] = useState(null)
+    const apiKey = process.env.REACT_APP_ACCU_API_KEY;
+
+async function fetchWeather(){
+    const result = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/351194?apikey=${apiKey}`)
+    const data = await result.json();
+    setWeather(data)
+}
+
+useEffect(() => {
+fetchWeather()
+},[location])
+
+
+return (
+    <div>
+        <input value={location} onChange={e => setLocation(e.target.value)}/>
+    <div>
+        {weather && JSON.stringify(weather, null, 2)}
+    </div>
+    </div>
+)
+}
+
+
+
+
+
+
+
 
 function LocationCard(props){
     const icon = getWeatherIcon(props.data.forecast)
